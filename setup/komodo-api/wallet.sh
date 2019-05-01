176072c1845e0b55b4efe76bc6d228db719bd80b
mylo <imylomylo@gmail.com>
2019-05-01 17:09:23 +0530

    shortcut for sending all to self

--- setup/komodo-api/wallet.sh
+++ setup/komodo-api/wallet.sh
@@ -38,3 +38,21 @@ function importprivkey {
     sleep 1
   fi
 }
+
+function sendalltoself {
+	KIABMETHOD="sendtoaddress"
+	if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+    source ~/.komodo/$CHAIN/$CHAIN.conf
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getbalance\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result' > ~/.kiabresponse
+	sleep 1
+	SENDAMOUNT=`cat ~/.kiabresponse`
+	echo $SENDAMOUNT
+	sleep 1
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"$DEVADDRESS\", $SENDAMOUNT, \"\", \"\", true]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    message_box "$KIABMETHOD response" "$KIABRESPONSE"
+  else
+    echo "Nothing to query - start devmode..."
+    sleep 1
+  fi
+}
