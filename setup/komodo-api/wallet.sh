6aac070195e90d2bf0547d20f37218713740abbe
mylo <imylomylo@gmail.com>
2019-05-01 17:16:53 +0530

    fix input

--- setup/komodo-api/wallet.sh
+++ setup/komodo-api/wallet.sh
@@ -61,7 +61,7 @@ function sendallto {
 	KIABMETHOD="sendtoaddress"
 	if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
     source ~/.komodo/$CHAIN/$CHAIN.conf
-    input_box "Send Question" "Send balance to which transparent address?" "1" SENDTOADDRESS
+    input_box "Send Question" "Send balance to which transparent address?" "address" SENDTOADDRESS
     curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getbalance\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.result' > ~/.kiabresponse
 	sleep 1
 	SENDAMOUNT=`cat ~/.kiabresponse`
