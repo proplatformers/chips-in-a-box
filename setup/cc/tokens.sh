5e64da39b9a4e9a0fbca2a964cb702bcf7cf16ae
mylo <imylomylo@gmail.com>
2019-05-12 20:05:21 +0700

    initial tokens shell

--- setup/cc/tokens.sh
+++ setup/cc/tokens.sh
@@ -1,8 +1,47 @@
+function tokenaddress {
+  KIABMETHOD="tokenaddress"
+  if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+    source ~/.komodo/$CHAIN/$CHAIN.conf
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.hex' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    message_box "$KIABMETHOD" "$KIABRESPONSE"
+  else
+    echo "Nothing to query - start $CHAIN..."
+    sleep 1
+  fi
+}
+
 function tokencreate {
   KIABMETHOD="tokencreate"
   if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
     source ~/.komodo/$CHAIN/$CHAIN.conf
     curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"GB\", 5, \"created GB token\"]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.hex' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    message_box "$KIABMETHOD" "$KIABRESPONSE"
+  else
+    echo "Nothing to query - start $CHAIN..."
+    sleep 1
+  fi
+}
+
+function tokenlist {
+  KIABMETHOD="tokenlist"
+  if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+    source ~/.komodo/$CHAIN/$CHAIN.conf
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.hex' > ~/.kiabresponse
+    KIABRESPONSE=`cat ~/.kiabresponse`
+    message_box "$KIABMETHOD" "$KIABRESPONSE"
+  else
+    echo "Nothing to query - start $CHAIN..."
+    sleep 1
+  fi
+}
+
+function tokeninfo {
+  KIABMETHOD="tokeninfo"
+  if ps aux | grep komodod | grep $CHAIN | grep -v grep ; then
+    source ~/.komodo/$CHAIN/$CHAIN.conf
+    curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"$KIABMETHOD\", \"params\": [\"$TXID\"""]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/ | jq -r '.hex' > ~/.kiabresponse
     KIABRESPONSE=`cat ~/.kiabresponse`
     message_box "$KIABMETHOD" "$KIABRESPONSE"
   else
