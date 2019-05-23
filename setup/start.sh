92c64daf535e3d59922c9ebcc1544b581a4c9d67
mylo <imylomylo@gmail.com>
2019-05-23 04:26:49 +0000

    check for enough memory, add 2GB swap file

--- setup/start.sh
+++ setup/start.sh
@@ -13,6 +13,40 @@ fi
 
 mkdir -p ~/bin
 
+
+SWAP_MOUNTED=$(cat /proc/swaps | tail -n+2)
+SWAP_IN_FSTAB=$(grep "swap" /etc/fstab || /bin/true)
+ROOT_IS_BTRFS=$(grep "\/ .*btrfs" /proc/mounts || /bin/true)
+TOTAL_PHYSICAL_MEM=$(head -n 1 /proc/meminfo | awk '{print $2}' || /bin/true)
+AVAILABLE_DISK_SPACE=$(df / --output=avail | tail -n 1)
+if
+	[ -z "$SWAP_MOUNTED" ] &&
+	[ -z "$SWAP_IN_FSTAB" ] &&
+	[ ! -e /swapfile ] &&
+	[ -z "$ROOT_IS_BTRFS" ] &&
+	[ $TOTAL_PHYSICAL_MEM -lt 5900000 ] &&
+	[ $AVAILABLE_DISK_SPACE -gt 15242880 ]
+then
+	echo "Adding a swap file to the system..."
+
+	# Allocate and activate the swap file. Allocate in 1KB chuncks
+	# doing it in one go, could fail on low memory systems
+	sudo dd if=/dev/zero of=/swapfile bs=2048 count=$[1024*1024] status=none
+	if [ -e /swapfile ]; then
+		sudo chmod 600 /swapfile
+		hide_output sudo mkswap /swapfile
+		sudo swapon /swapfile
+	fi
+
+	# Check if swap is mounted then activate on boot
+	if sudo swapon -s | grep -q "\/swapfile"; then
+		echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab > /dev/null
+	else
+		echo "ERROR: Swap allocation failed"
+	fi
+fi
+
+
 # Put a start script in a global location. We tell the user to run 'chipsinabox'
 # in the first dialog prompt, so we should do this before that starts.
 cat > ~/bin/chipsinabox << EOF;
